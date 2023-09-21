69af8d9684737d3005154a71bca90fd328e4e7b8
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-09-21 07:40:06 +0530

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -25,7 +25,7 @@ async def stats(e):
 async def dl_link(event):
     if not event.is_private:
         return
-    if str(event.sender_id) not in OWNER and event.sender_id !=DEV:
+    if event.sender_id != OWNER and event.sender_id not in DEV:
         return
     link, name = "", ""
     try:
@@ -120,7 +120,7 @@ async def encod(event):
         if not event.is_private:
             return
         event.sender
-        if str(event.sender_id) not in OWNER and event.sender_id !=DEV:
+        if event.sender_id != OWNER and event.sender_id not in DEV:
             return await event.reply("**Sorry You're not An Authorised User!**")
         if not event.media:
             return
